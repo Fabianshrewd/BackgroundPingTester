52e9d8c542922956de88801d47511a7ac5b501b5
Fabian <fabiangranig.fg@gmail.com>
2023-02-06 12:14:59 +0100

    Added all files

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace BackgroundPingTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox_overview = new System.Windows.Forms.RichTextBox();
+            this.textBox_average = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "BackGroundPingTester";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // textBox_overview
+            // 
+            this.textBox_overview.Location = new System.Drawing.Point(12, 12);
+            this.textBox_overview.Name = "textBox_overview";
+            this.textBox_overview.Size = new System.Drawing.Size(644, 426);
+            this.textBox_overview.TabIndex = 3;
+            this.textBox_overview.Text = "";
+            // 
+            // textBox_average
+            // 
+            this.textBox_average.Location = new System.Drawing.Point(676, 12);
+            this.textBox_average.Name = "textBox_average";
+            this.textBox_average.Size = new System.Drawing.Size(100, 20);
+            this.textBox_average.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_average);
+            this.Controls.Add(this.textBox_overview);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BackGroundPingTester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.RichTextBox textBox_overview;
+        private System.Windows.Forms.TextBox textBox_average;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace BackgroundPingTester
+{
+    public partial class Form1 : Form
+    {
+        public long[] arr;
+        public int i;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            arr = new long[10];
+            for(int u = 0; u < arr.Length; u++)
+            {
+                arr[u] = 1;
+            }
+            i = 0;
+            Thread t1 = new Thread(whilePingTest);
+            t1.Start();
+        }
+
+        private void whilePingTest()
+        {
+            while(true)
+            {
+                PingTest();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void PingTest()
+        {
+            Ping ping = new Ping();
+            PingReply reply = ping.Send("google.at", 10000);
+
+            try
+            {
+                textBox_overview.BeginInvoke((Action)delegate
+                {
+                    textBox_overview.Text += "Status :  " + reply.Status + " Time : " + reply.RoundtripTime.ToString() + " Address : " + reply.Address + "\n";
+                });
+
+                arr[i] = reply.RoundtripTime;
+                i++;
+
+                if(i > 9)
+                {
+                    Thread.Sleep(5000);
+
+                    i = 0;
+                    textBox_overview.BeginInvoke((Action)delegate
+                    {
+                        textBox_overview.Text = "";
+                    });
+                }
+
+                long sum = 0;
+                for(int u = 0; u < arr.Length; u++)
+                {
+                    sum += arr[u];
+                }
+
+                textBox_average.BeginInvoke((Action)delegate
+                {
+                    textBox_average.Text = "Avg: " + Convert.ToString(sum / arr.Length);
+                });
+                
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+    }
+}
